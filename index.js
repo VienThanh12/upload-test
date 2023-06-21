52395aad0912c2017ffa24d2ca0f1bce128e4857
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-06-21 16:04:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.delete('/info/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
